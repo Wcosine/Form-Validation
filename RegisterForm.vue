7655fdff2ee54a0911853e7f15c71263ed82d3b8
Wcosine <132649565+Wcosine@users.noreply.github.com>
2025-05-26 17:52:02 +0800

    Update and rename Register.vue to RegisterForm.vue
    v1 to v2
    修改驗證邏輯的寫法, 並加強使用者名稱驗證邏輯

--- RegisterForm.vue
+++ RegisterForm.vue
@@ -1,4 +1,4 @@
-<!-- 這是v1版本 -->
+<!-- 這是v2版本 -->
 
 <script setup>
 import {ref} from 'vue'
@@ -19,41 +19,63 @@ const isVaildEmail = (email) =>{
   return pattern.test(email)
   //這是一個function, 要記得return才會回傳測試結果
 }
+const isEnglishOnly = (text) => {
+  const pattern = /^[A-Za-z0-9]+$/
+  return pattern.test(text)
+}
 
-const confirmRegister = () =>{
-  userNameError.value = ""
-  emailError.value = ""
-  passwordError.value = ""
-  passwordConfirmError.value = ""
-
+// 將驗證邏輯分離更好維護
+const validateUser = () =>{
   if(userName.value === ""){
     userNameError.value = "請輸入使用者名稱"
+  } else if(userName.value.length < 5 ){
+    userNameError.value = '使用者名稱至少5字'
+  } else if(isEnglishOnly(userName.value)){
+    userNameError.value = '使用者名稱僅能為英文字母及數字'
+  } else {
+    userNameError.value = ""
   }
+}
+const validateEmail = () =>{
   if(email.value === ""){
-    emailError.value = "請輸入電子郵件"
+    emailError.value = "請輸入郵件"
   } else if(!isVaildEmail(email.value)){
     emailError.value = '請輸入正確格式'
+  } else {
+    emailError.value = ""
   }
+}
+const validatePassword = () =>{
   if(password.value === ""){
-    emailError.value = "請輸入密碼"
+    passwordError.value = "請輸入密碼"
   } else if(password.value.length < 6){
     passwordError.value = '密碼長度至少6字'
+  } else {
+    passwordError.value = ""
   }
-  if(passwordConfirm.value === ""){
+}
+const validatePasswordConfirm = () =>{
+  if(password.value === ""){
     passwordConfirmError.value = "再次輸入密碼"
   } else if (passwordConfirm.value !== password.value){
     passwordConfirmError.value = "請確認密碼是否一致"
+  } else {
+    passwordConfirmError.value = ""
   }
+}
+
+const confirmRegister = () =>{
+  validateUser()
+  validateEmail()
+  validatePassword()
+  validatePasswordConfirm()
   if(userNameError.value === "" &&
      emailError.value === "" && 
      passwordError.value === "" &&
      passwordConfirmError.value === "" 
      ){
-    alert ('成功註冊')
-    userName.value = ""
-    email.value = ""
-    password.value = ""
-    passwordConfirm.value = ""
+    alert ('成功註冊') 
+    reset()
   }
 }
 const reset = () =>{
@@ -73,7 +95,9 @@ const reset = () =>{
   <h2>使用者註冊</h2>  
   <FormInput>
     <template #userName>
-      <label class="register-item"> 使用者名稱 <input type="text" v-model="userName" placeholder=""/></label>
+      <label class="register-item"> 使用者名稱 <input type="text"  placeholder="僅能輸入英文及數字" 
+      v-model="userName" @input="userName = userName.replace(/[^A-Za-z0-9]/g, '')"/></label>
+      <!-- 控制輸入的內容僅能為英文及數字, 註1 -->
       <p v-if="userNameError" style="color:red">{{userNameError}}</p>
     </template>
     <template #email>
@@ -94,3 +118,8 @@ const reset = () =>{
     </template> 
   </FormInput> 
 </template>
+
+<!--
+    #補充說明
+    1. /[^A-Za-z0-9]/g 是正規表達式, "^"表示非, 這裡指非英文及數字的內容, "/g"代表 global（全域搜尋）, 逗號後的''表示替換成空
+-->
