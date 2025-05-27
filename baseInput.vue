d444b3f8aa4df2d14643ead83fe775454e3f6752
Wcosine <132649565+Wcosine@users.noreply.github.com>
2025-05-27 22:51:37 +0800

    Create baseInput.vue

---
+++ baseInput.vue
@@ -0,0 +1,47 @@
+<!-- 這個是將input欄位獨立為元件的檔案, 且不使用slot的製作方式 -->
+<!-- 這是v1版本, 配合newRegiterForm.vue -->
+
+<script setup>
+import {defineProps, defineEmits} from 'vue'
+const prop = defineProps({
+  label: String,
+  placeholder: String,
+  type:{
+    type: String,
+    default: 'text',
+  },
+  modelValue: String
+})
+// 由父組件控制值
+
+const emit = defineEmits(["update:modelValue"])
+// 這個emit目的是發出事件給父組件知道要更新
+const updateValue = (e) =>{
+  // e只是一個變數值, 可以自定義, e通常是event的縮寫
+  emit("update:modelValue", e.target.value)
+}
+
+</script>
+
+<template>
+  <label class="inputItem" >
+    {{label}}
+    <input
+    :type="type"
+    :placeholder="placeholder"
+    :value="modelValue"
+    @input="updateValue"/>
+    <!--當有input資料更新的時候觸發emit-->
+  </label>
+</template>
+
+<style scoped>
+.inputItem{
+  display: flex;
+  margin: 1rem;
+  background-color: #ccc;
+}
+input{
+  margin-left: 12px;
+}
+</style>
